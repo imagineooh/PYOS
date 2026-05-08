0185ed85f4a37ecbc4fe32b9e451ef0db9d7da2e
imagineooh <flaviocodes1916@gmail.com>
2026-05-08 19:18:58 +0200

    added free checking to add_inode function in inode.py

--- Virtual Machine OS/Pyos/File system/inode.py
+++ Virtual Machine OS/Pyos/File system/inode.py
@@ -6,12 +6,13 @@ class Inode:
         self.ram.sign_in('F', 'pas')
         self.ram.add_user('F', 'pas')
     def add_inode(self, address: int, type_file:str, filename: str):
-        if type_file=='file':
-            if self.ram.write(address, [(self.counter, type_file, filename), None]) != "Data adress already taken, try 'force_write' method (unsafe) or 'free_index' method first. ...//":
-                self.ram.write(address, [(self.counter, type_file, filename), None])
-        elif type_file=='folder':
-            if self.ram.write(address, [(self.counter, type_file, filename), []])  != "Data adress already taken, try 'force_write' method (unsafe) or 'free_index' method first. ...//":
-                self.ram.write(address, [(self.counter, type_file, filename), []])
+        if type_file=='file' and self.ram[address]==0:
+            if self.ram.write(address, [(self.counter, type_file, filename), None], True) != "Data adress already taken, try 'force_write' method (unsafe) or 'free_index' method first. ...//":
+                self.ram.write(address, [(self.counter, type_file, filename), None], True)
+
+        elif type_file=='folder' and self.ram[address]==0:
+            if self.ram.write(address, [(self.counter, type_file, filename), []], True)  != "Data adress already taken, try 'force_write' method (unsafe) or 'free_index' method first. ...//":
+                self.ram.write(address, [(self.counter, type_file, filename), []], True)
                 self.filename_index[filename]=address
         self.counter+=1
 
