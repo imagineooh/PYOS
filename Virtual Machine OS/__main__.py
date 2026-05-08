7ddf2d6ed761c4755499e30dfffea6f301e0cc00
imagineooh <flaviocodes1916@gmail.com>
2026-05-08 19:20:00 +0200

    added new checking routine to __main__.py

--- Virtual Machine OS/__main__.py
+++ Virtual Machine OS/__main__.py
@@ -1,11 +1,13 @@
 from ram import RAM
 from filesystem import FileSystem
-
+from directory import Directory
 
 ram=RAM(16)
 file_manager=FileSystem(ram)
+directory_manager=Directory(ram)
 
-
-file_manager.construct_folder('test',[1,5,6], 5)
-file_manager.construct_folder('test2', [1,6,1,5,7,9,8,4], 6)
+directory_manager.add_empty_folder('test', [1,45,988,77,4], 5)
+directory_manager.add_empty_folder('test2', [5,8,9,4,4], 6)
+directory_manager.add_empty_folder('test3', [5,8,9,4,4], 5)
 print(ram)
+
