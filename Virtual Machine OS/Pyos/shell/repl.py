fa12188ebab2e61e588076d5dbe81bec5792c6b6
imagineooh <flaviocodes1916@gmail.com>
2026-05-19 14:19:39 +0200

    V1.5EDIT :  repl no longer needs cmd module

--- Virtual Machine OS/Pyos/shell/repl.py
+++ Virtual Machine OS/Pyos/shell/repl.py
@@ -7,7 +7,7 @@ import inspect
 
 ram=RAM(16)
 storage=Storage(ram)
-class TameShell(Cmd):
+class TameShell():
     username = 'flav'
     intro = "Welcome to TameOS! Type help or ? to list commands.\n"
     prompt = f"C://TameOS/user:{username}/terminal: "
@@ -26,44 +26,42 @@ class TameShell(Cmd):
         }
 
     def end(self):
-        return 1
+        print("Quitting TameOS. See you soon!")
+        return True
 
     def default(self,arg):
         args = arg.split()
         keyword = str(args[0])
-        func = self.commands_dict[keyword]
+        if keyword in self.commands_dict:
+            func = self.commands_dict[keyword]
 
-        call_args = []
-        if len(args) > 1:
-            call_args.append(args[1])
-        if len(args) > 2:
-            call_args.append(args[2].split(","))
-        if len(args) > 3:
-            call_args.append(int(args[3]))
+            call_args = []
+            if len(args) > 1:
+                call_args.append(args[1])
+            if len(args) > 2:
+                call_args.append(args[2].split(","))
+            if len(args) > 3:
+                call_args.append(int(args[3]))
 
 
-        try:
-            sig = inspect.signature(func)
-            bound = sig.bind_partial(*call_args)
-            bound.apply_defaults()
-            return func(*bound.args, **bound.kwargs)
-        except TypeError as e:
-            print(f"Arg error {e}")
-            return None
+            try:
+                sig = inspect.signature(func)
+                bound = sig.bind_partial(*call_args)
+                bound.apply_defaults()
+                return func(*bound.args, **bound.kwargs)
+            except TypeError as e:
+                print(f"Arg error {e}")
+                return None
+
+    def loop(self):
+        while True:
+            try:
+                arg = input(TameShell.prompt)
+                if self.default(arg):
+                    break
+            except EOFError:
+                break
 
 shell = TameShell(ram, storage)
-shell.preloop()
-
-line=print(shell.intro)
-while True:
-    try:
-        line = input(shell.prompt)
-        stop = shell.onecmd(line)
-        shell.postcmd(stop, line)
-        if stop:
-            break
-    except EOFError:
-        break
-
-shell.postloop()
+shell.loop()
 
