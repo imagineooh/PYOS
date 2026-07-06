c25e691bb3891f6f12d90b8c9821418db8354338
imagineooh <flaviocodes1916@gmail.com>
2026-07-06 13:13:22 +0200

    FEAT added differentiation for variable type (const, var) in compiler.py

--- Virtual Machine OS/Pyos/editor/compiler.py
+++ Virtual Machine OS/Pyos/editor/compiler.py
@@ -14,6 +14,7 @@ class Compiler:
         }
         self.mapper = {
             "SET":self.__do_set,
+            "CONST": self.__do_set,
             "OP":self.__do_op,
         }
         self.variable_status={}
@@ -34,21 +35,35 @@ class Compiler:
                 self.lines:list = [line.strip() for line in file]
         else:
             #self.lines: list = ["OP 2*3-2", "OP 12*456-2", "OP ((237232/4544-2*5)+2)/4"]
-            self.lines: list = ["SET x=3*(-12*4*(5- 6))-(18*(4+2))/5"]
+            self.lines: list = ["CONST x=3*(-12*4*(5- 6))-(18*(4+2))/5",
+                                "CONST x=5"]
         for i,value in enumerate(self.lines):
             keyword = value.split()[0]
             self.mapper[keyword](i, len(keyword))
 
 
-    def __do_set(self, line_number:str, keyword_len:int = 3):
+    def __do_set(self, line_number:str, keyword_len:int = 3) ->None:
+        """
+        Hidden setter method for adding variable in memory from compiler
+        :param line_number: str, line read for setting
+        :param keyword_len: len of the keyword arg
+        :param constarg: boolean for const status of var, True for const, False for simple var
+        :return: None
+        """
         line = self.lines[line_number]
         body = line[keyword_len:]
         tokens = body.split("=", 1)
         variable_name = tokens[0].strip()
-        self.variable_status[variable_name] = "const"
-        #variable_expression = tokens[1].strip()
+        if variable_name in self.variable_status.keys():
+            if self.variable_status[variable_name]=="const":
+                return
+        keyword=line[:keyword_len]
+        print(keyword)
+        if keyword=="CONST":
+            self.variable_status[variable_name] = "const"
+        else:
+            self.variable_status[variable_name] = "var"
         offset = keyword_len + len(variable_name)+2
-        #print(f"offsetted to {line[offset:]}")
         variable_value = self.__do_op(line_number, offset)
         #self.directory_manager.add_empty_folder(variable_name, variable_value, 0)
         print(f"{variable_name} = {variable_value}")
