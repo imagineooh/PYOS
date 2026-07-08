0856340e52c5546b36dc78063af2afdff3c1d7db
imagineooh <flaviocodes1916@gmail.com>
2026-07-08 19:10:14 +0200

    TESTER added a copypastable input sequence to check if outputs are correct

--- Virtual Machine OS/main.py
+++ Virtual Machine OS/main.py
@@ -5,13 +5,15 @@ from ram import RAM
 from storage import Storage
 import logging
 
-if __name__ == "__main__":
+def main():
     logging.basicConfig(level=logging.INFO, filename="TameOSlog.log", filemode='w',
                         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     stdout_handler = logging.StreamHandler(sys.stdout)
     stdout_handler.setLevel(logging.ERROR)
     logging.getLogger().addHandler(stdout_handler)
-    ram=RAM(1024)
-    storage=Storage(ram)
+    ram = RAM(1024)
+    storage = Storage(ram)
     shell = TameShell(ram, storage)
     shell.loop()
+if __name__ == "__main__":
+    main()
